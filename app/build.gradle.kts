3a3b9b4356323cfea482e4fc13c659e6412357c7
greenpower2669 <80179295+greenpower2669@users.noreply.github.com>
2026-09-08 22:46:27 +0200

    chore: bump prototype to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gambitdrame.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.1"
+        versionCode = 3
+        versionName = "0.2.2"
     }
 
     buildTypes {
